48bba5ab5b48188944a2a29f5108bad3a595ec37
JD Mauthe <jd@jdmauthe.com>
2023-11-14 11:08:32 -0800

    initialize phoenix project

---
+++ lib/anon_roulette/mailer.ex
@@ -0,0 +1,3 @@
+defmodule AnonRoulette.Mailer do
+  use Swoosh.Mailer, otp_app: :anon_roulette
+end
---
+++ lib/anon_roulette_web/controllers/page_html.ex
@@ -0,0 +1,5 @@
+defmodule AnonRouletteWeb.PageHTML do
+  use AnonRouletteWeb, :html
+
+  embed_templates "page_html/*"
+end
